9c0d78309934557ef90338e0bf1ffcef059cbe6b
Md Arif <arif.20527@sscbs.du.ac.in>
2024-10-16 20:12:12 +0530

    checking push

--- src/app/Components/InfiniteScroll.jsx
+++ src/app/Components/InfiniteScroll.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { cn } from "@/lib/utils";
+import { cn } from "../lib/utils";
 import React, { useEffect, useState } from "react";
 
 export const InfiniteScroll = ({
@@ -8,7 +8,7 @@ export const InfiniteScroll = ({
   direction = "left",
   speed = "fast",
   pauseOnHover = true,
-  className
+  className,
 }) => {
   const containerRef = React.useRef(null);
   const scrollerRef = React.useRef(null);
@@ -32,13 +32,19 @@ export const InfiniteScroll = ({
       getSpeed();
       setStart(true);
     }
-  }
+  }  
   const getDirection = () => {
     if (containerRef.current) {
       if (direction === "left") {
-        containerRef.current.style.setProperty("--animation-direction", "forwards");
+        containerRef.current.style.setProperty(
+          "--animation-direction",
+          "forwards"
+        );
       } else {
-        containerRef.current.style.setProperty("--animation-direction", "reverse");
+        containerRef.current.style.setProperty(
+          "--animation-direction",
+          "reverse"
+        );
       }
     }
   };
@@ -54,19 +60,21 @@ export const InfiniteScroll = ({
     }
   };
   return (
-    (<div
+    <div
       ref={containerRef}
       className={cn(
         "scroller relative z-20  max-w-7xl overflow-hidden  [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
         className
-      )}>
+      )}
+    >
       <ul
         ref={scrollerRef}
         className={cn(
           " flex min-w-full shrink-0 gap-4 py-4 w-max flex-nowrap",
           start && "animate-scroll ",
           pauseOnHover && "hover:[animation-play-state:paused]"
-        )}>
+        )}
+      >
         {items.map((item) => (
           <li
             className="w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-8 py-6 md:w-[450px]"
@@ -74,13 +82,14 @@ export const InfiniteScroll = ({
               background:
                 "linear-gradient(180deg, var(--slate-800), var(--slate-900)",
             }}
-            key={item.name}>
+            key={item.name}
+          >
             <blockquote>
               <div
                 aria-hidden="true"
-                className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"></div>
-              <span
-                className=" relative z-20 text-sm leading-[1.6] text-gray-100 font-normal">
+                className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
+              ></div>
+              <span className=" relative z-20 text-sm leading-[1.6] text-gray-100 font-normal">
                 {item.quote}
               </span>
               <div className="relative z-20 mt-6 flex flex-row items-center">
@@ -97,6 +106,6 @@ export const InfiniteScroll = ({
           </li>
         ))}
       </ul>
-    </div>)
+    </div>
   );
 };
