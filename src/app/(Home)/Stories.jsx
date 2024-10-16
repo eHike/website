aada83d71515674cb142b338360364f8a1907f8c
Md Arif <arif.20527@sscbs.du.ac.in>
2024-10-16 19:26:20 +0530

    commiting first

---
+++ src/app/(Home)/Stories.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { InfiniteScroll } from '../../Components/InfiniteScroll'
+const Stories = () => {
+    const items=[{name:"Arif",title:"title1"},{name:"Sudip",title:"title2"},{name:"Arifa",title:"title1"},{name:"Sudipa",title:"title2"},{name:"Arifz",title:"title1"},{name:"Sudipz",title:"title2"},{name:"Arif2",title:"title1"},{name:"Sudip2",title:"title2"}]
+  return (
+    <div>Stories
+    <InfiniteScroll items={items}/>
+    </div>
+  )
+}
+
+export default Stories
